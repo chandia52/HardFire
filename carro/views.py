5031b05645f1397c610b116ba627af2a64cb7215
chandia52 <jorgechandia7@gmail.com>
2024-06-19 01:53:51 -0300

    Se elimino Sweet Alert.. Se incorporo la funcion agregar al carrito en todos los listados de los productos y el detail de cada uno.. Se soluciono problema que dificultaba la visualizacion de las listas en el carro cuando estaban vacias

--- carro/views.py
+++ carro/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render,redirect
 from .carro import Carro
 from productos.models import Producto
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-
+@login_required
 def agregar_producto(request,producto_id):
     carro = Carro(request)
     producto = Producto.objects.get(id=producto_id)
@@ -13,7 +14,7 @@ def agregar_producto(request,producto_id):
 
     return redirect("carro:carro")
 
-
+@login_required
 def eliminar_producto(request,producto_id):
     carro= Carro(request)
     producto= Producto.objects.get(id=producto_id)
@@ -23,7 +24,7 @@ def eliminar_producto(request,producto_id):
     return redirect("carro:carro")
 
 
-
+@login_required
 def restar_producto(request,producto_id):
     carro= Carro(request)
     producto= Producto.objects.get(id=producto_id)
@@ -32,14 +33,14 @@ def restar_producto(request,producto_id):
     
     return redirect("carro:carro")
 
-
+@login_required
 def limpiar_carroproducto(request):
     carro= Carro(request)
     carro.limpiar_carrito()
     
     return redirect("carro:carro")
 
-
+@login_required
 def carro_detalle(request):
     carro = Carro(request)
     productos_en_carro = carro.carro.items()
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,22 +2,3 @@
 const change = src => {
 document.getElementById('main').src = src
 };
-
-
-
-const addtocart=() =>{
-    Swal.fire({
-        icon: "success",
-        title: "Se ha agregado su articulo al carrito exitosamente",
-        showConfirmButton: true,
-        timer: 3500
-    });
-}
-
-const empty=()=>{
-    Swal.fire({
-        title: "No hay productos?",
-        text: "Por favor seleccione algun producto",
-        icon: "info"
-    });
-}
